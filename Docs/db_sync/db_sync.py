c474edab4040e8f7e2120c90aba3accb1b356acb
Peter Bailie <pbailie@users.noreply.github.com>
2017-10-25 20:13:49 -0400

    Changes
    File: Docs/db_sync/db_sync.py
    
    * Rewriting script as a class
    * Code comments

--- Docs/db_sync/db_sync.py
+++ Docs/db_sync/db_sync.py
@@ -6,68 +6,94 @@ import os
 import psycopg2
 import sys
 
-
 # CONFIGURATION ----------------------------------------------------------------
 DB_HOST = 'localhost'
 DB_USER = 'hsdbu'
 DB_PASS = 'hsdbu'
 # ------------------------------------------------------------------------------
 
-def main(argv):
-	if len(argv) < 2 or argv[1] == 'help':
-		print_help()
-		sys.exit(0)
-	elif argv[1] == 'all':
-		# Connect to DB
-		db_conn = db_connect()
-		# Get complete course list
-		course_list = get_all_courses(db_conn)
-	else:
-		# Connect to DB
-		db_conn = db_connect()
-		# Validate list
-		# If validated, sync all courses in list
+class db_sync:
 
-	# Process database sync
-	# Exit
+	def __init__(self):
 
+		if len(sys.argv) < 2 or sys.argv[1] == 'help':
+			self.print_help()
+			sys.exit(0)
+		elif sys.argv[1] == 'all':
+			# Get complete course list
+			self.db_connect()
+			course_list = self.get_all_courses()
+		else:
+			self.db_connect()
+			if sys.argv[1] in self.get_all_courses():
+				course_list = [sys.argv[1]]
+			else:
+				raise SystemExit("Error: '{}' not a valid course.".format(sys.argv[1]))
 
-def db_connect():
-	try:
-		return psycopg2.connect("dbname='submitty' user={} host={} password={}".format(DB_USER, DB_HOST, DB_PASS))
-	except:
-		raise SystemExit("ERROR: Cannot connect to Submitty master database")
+		print(course_list)
 
-def get_all_courses(db_conn):
-	db_cur = db_conn.cursor()
-	db_cur.execute("SELECT course FROM courses WHERE semester='{}'".format(determine_semester()))
-	return [row[0] for row in db_cur.fetchall()]
+		# Process database sync
+		# Exit
 
-def determine_semester():
-	today = datetime.date.today()
-	month = today.month
-	year  = str(today.year % 100)
-	# if month <= 5: ... elif month >=8: ... else: ...
-	return 's' + year if month <= 5 else ('f' + year if month >= 8 else 'm' + year)
+# ------------------------------------------------------------------------------
 
-def print_help():
-	"""
-	Print help message to STDOUT/console
-	"""
-	os.system('clear')
-	print("Usage: db_sync.py (help | all | course...)\n");
-	print("Command line tool to sync course databases with master submitty database.\n")
-	print("help:   This help message")
-	print("all:    Sync all course databases")
-	print("course: Specific course or list of courses to sync\n")
-	print("EXAMPLES:")
-	print("db_sync.py all")
-	print("Sync ALL courses with master submitty database.\n")
-	print("db_sync.py csci1100")
-	print("Sync course csci1100 with master submitty databse.\n")
-	print("db_sync.py csci1200 csci2200 csci3200")
-	print("Sync courses csci1200, csci2200, and csci3200 with master submitty database.\n")
+	def db_connect(self):
+		"""Establish connection to Submitty Master DB"""
+
+		try:
+			self.DB_CONN = psycopg2.connect("dbname='submitty' user={} host={} password={}".format(DB_USER, DB_HOST, DB_PASS))
+		except:
+			pass
+			#raise SystemExit("ERROR: Cannot connect to Submitty master database")
+
+# ------------------------------------------------------------------------------
+
+	def get_all_courses(self):
+		"""
+		Retrieve active course list from Master DB
+		:return: list of all active courses
+		:rtype: list (string)
+		"""
+
+		db_cur = self.DB_CONN.cursor()
+		db_cur.execute("SELECT course FROM courses WHERE semester='{}'".format(self.determine_semester()))
+		return [row[0] for row in db_cur.fetchall()]
+
+# ------------------------------------------------------------------------------
+
+	def determine_semester(self):
+		"""
+		Build/return semester string.  e.g. "s17" for Spring 2017.
+		:return: The semester string
+		:rtype: string
+		"""
+
+		today = datetime.date.today()
+		month = today.month
+		year  = str(today.year % 100)
+		# if month <= 5: ... elif month >=8: ... else: ...
+		return 's' + year if month <= 5 else ('f' + year if month >= 8 else 'm' + year)
+
+# ------------------------------------------------------------------------------
+
+	def print_help(self):
+		"""Print help message to STDOUT/console"""
+
+		os.system('clear')
+		print("Usage: db_sync.py (help | all | course...)\n");
+		print("Command line tool to sync course databases with master submitty database.\n")
+		print("help:   This help message")
+		print("all:    Sync all course databases")
+		print("course: Specific course or list of courses to sync\n")
+		print("EXAMPLES:")
+		print("db_sync.py all")
+		print("Sync ALL courses with master submitty database.\n")
+		print("db_sync.py csci1100")
+		print("Sync course csci1100 with master submitty databse.\n")
+		print("db_sync.py csci1200 csci2200 csci3200")
+		print("Sync courses csci1200, csci2200, and csci3200 with master submitty database.\n")
+
+# ------------------------------------------------------------------------------
 
 if __name__ == "__main__":
-	main(sys.argv)
-	sys.exit(0)
+	db_sync()
